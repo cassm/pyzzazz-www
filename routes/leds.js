d5230615e760fb21dc9c3d9080ea1a25cad40d39
Cass May <cass@cassm.net>
2022-02-23 15:43:17 +0000

    serve remaining redis state objects

--- routes/leds.js
+++ routes/leds.js
@@ -21,12 +21,27 @@ router.get('/:resource', async function(req, res, next) {
 
   let value = {};
 
-  if (resource === 'coords') {
-    value = await JSON.parse(await r.get('pyzzazz:leds:coords'));
-  } else if (resource === 'colours') {
-    value = await JSON.parse(await r.get('pyzzazz:leds:colours'));
-  } else {
-    winston.info(`Request for invalid resource "${resource}"`);
+  switch (resource) {
+    case 'coords':
+      value = await JSON.parse(await r.get('pyzzazz:leds:coords'));
+      break;
+    case 'colours':
+      value = await JSON.parse(await r.get('pyzzazz:leds:colours'));
+      break;
+    case 'fixtures':
+      value = await JSON.parse(await r.get('pyzzazz:fixtures'));
+      break;
+    case 'patterns':
+      value = await JSON.parse(await r.get('pyzzazz:patterns'));
+      break;
+    case 'palettes':
+      value = await JSON.parse(await r.get('pyzzazz:palettes'));
+      break;
+    case 'overlays':
+      value = await JSON.parse(await r.get('pyzzazz:overlays'));
+      break;
+    default:
+      winston.info(`Request for invalid resource "${resource}"`);
   }
 
   res.json(value);
