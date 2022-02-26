9f25026a655378c5522488c08d9075ddeff00d88
Cass May <cass@cassm.net>
2022-02-26 15:56:43 +0000

    Display svg logo

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import { Logo } from './logo.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo width={300} height={300} strokeWidth={10} />
+        <p id="logo-text">Pyzzazz</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
