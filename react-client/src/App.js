c96325b2762f042fd66e8ef07a86f100fc168966
Cass May <cass@cassm.net>
2022-03-01 18:02:20 +0000

    use correct modern react router syntax
    i'm still not used to how fast apis change in the webdev world. at my last job i was happily using headers with ARPA in the title which still used the original apis. whinge complain moan.

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,5 +1,5 @@
-import Logo from './logo.js';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Routes} from 'react-router-dom';
 import Header from './Header.js';
 import Configuration from './Configuration.js';
 import Control from './Control.js';
@@ -8,37 +8,19 @@ import Visualiser from './Visualiser.js';
 
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Header/>
-        <Route path="/configuration">
-          <Configuration/>
-        </Route>
-        <Route path="/control">
-          <Control/>
-        </Route>
-        <Route path="/status">
-          <Status/>
-        </Route>
-        <Route path="/visualiser">
-          <Visualiser/>
-        </Route>
-        <header className="App-header">
-          <p id="logo-text">Pyzzazz</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Routes>
+          <Route path="/configuration" element={<Configuration/>}/>
+          <Route path="/control" element={<Control/>}/>
+          <Route path={"/status"} element={<Status/>}/>
+          <Route path={"/"} element={<Status/>}/>
+          <Route path="/visualiser" element={<Visualiser/>}/>
+        </Routes>
       </Router>
     </div>
   );
