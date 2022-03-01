e9e02357dc49d90c9d1d8a3c0dd90dd302a7c957
Cass May <cass@cassm.net>
2022-03-01 18:03:23 +0000

    draw header and logo on status page

--- react-client/src/Status.js
+++ react-client/src/Status.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import Logo from "./logo";
 
 const Status = props => {
+  return (
+    <div>
+      <h1>Status</h1>
+
+      <Logo size="300" darkMode={false} key="statusLogo"/>
+    </div>
+  )
 }
 
 export default Status;
