2339da1946ebd4e1b04d2e96f7e9f9e448c821fc
Cass May <cass@cassm.net>
2022-03-03 17:47:31 +0000

    regretfully remove big logo from status page

--- react-client/src/Status.js
+++ react-client/src/Status.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Logo from "./logo";
 import FpsView from './FpsView';
 import './Status.css';
 
@@ -15,8 +14,6 @@ const Status = props => {
   return (
     <div>
       <h1>Status</h1>
-
-      <Logo size="300" darkMode={false} key="statusLogo"/>
       <FpsView width="40%" height={100} resourcePath='/resource/fps' viewName='Pyzzazz server'/>
       <h3>{props.loading ? 'Loading...' : `Mapping ${props.coords.length} LEDs over ${fixtureInstances.length} fixtures. No big deal ;-)`}</h3>
 
