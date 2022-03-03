6c2c1226aa8db317bfba0bd0e673b8830e944517
Cass May <cass@cassm.net>
2022-03-03 17:45:22 +0000

    load coords and fixture list and pass to status page

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Routes} from 'react-router-dom';
+import {useEffect, useState} from 'react';
 import NavBar from './NavBar.js';
 import Configuration from './Configuration.js';
 import Control from './Control.js';
@@ -10,6 +11,29 @@ import './App.css';
 
 
 function App() {
+  const [coords, setCoords] = useState([]);
+  const [fixtures, setFixtures] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getResources() {
+      async function fetchResource (resource) {
+        const res = await fetch(`/resource/${resource}`);
+        return await res.json();
+      }
+
+      setLoading(true);
+      const res_coords = await fetchResource('coords');
+      const res_fixtures = await fetchResource('fixtures');
+
+      setCoords(res_coords);
+      setFixtures(res_fixtures);
+      setLoading(false);
+    }
+
+    getResources();
+  }, [])
+
   return (
     <div className="App">
       <Router>
@@ -17,8 +41,8 @@ function App() {
         <Routes>
           <Route path="/configuration" element={<Configuration/>}/>
           <Route path="/control" element={<Control/>}/>
-          <Route path={"/status"} element={<Status/>}/>
-          <Route path={"/"} element={<Status/>}/>
+          <Route path={"/status"} element={<Status coords={coords} fixtures={fixtures} loading={loading}/>}/>
+          <Route path={"/"} element={<Status coords={coords} fixtures={fixtures} loading={loading}/>}/>
           <Route path="/visualiser" element={<Visualiser/>}/>
         </Routes>
       </Router>
--- react-client/src/Status.js
+++ react-client/src/Status.js
@@ -4,12 +4,21 @@ import FpsView from './FpsView';
 import './Status.css';
 
 const Status = props => {
+  const fixtureInstances = [];
+
+  if (!props.loading) {
+    for (const [_, entry] of Object.entries(props.fixtures['Fixture']['LedFixture'])) {
+      fixtureInstances.push(...entry.instances);
+    }
+  }
+
   return (
     <div>
       <h1>Status</h1>
 
       <Logo size="300" darkMode={false} key="statusLogo"/>
       <FpsView width="40%" height={100} resourcePath='/resource/fps' viewName='Pyzzazz server'/>
+      <h3>{props.loading ? 'Loading...' : `Mapping ${props.coords.length} LEDs over ${fixtureInstances.length} fixtures. No big deal ;-)`}</h3>
 
     </div>
   )
