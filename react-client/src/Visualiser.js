e0813f6f92d238cbe1a261a01f4e26e2e9d835e7
Cass May <cass@cassm.net>
2022-03-04 11:28:16 +0000

    add orbitcontrols to visualiser

--- react-client/src/Visualiser.js
+++ react-client/src/Visualiser.js
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom'
 import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useThree, extend } from '@react-three/fiber'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import {v4 as uuidv4} from 'uuid';
 import './Visualiser.css';
+
+// Extend will make OrbitControls available as a JSX element called orbitControls for us to use.
+extend({ OrbitControls });
 
 function Led (props) {
   return (
@@ -15,9 +19,20 @@ function Led (props) {
   );
 }
 
+const CameraControls = () => {
+  const {
+    camera,
+    gl: {domElement},
+  } = useThree();
+  const controls = useRef();
+  useFrame((state) => controls.current.update());
+  return <orbitControls ref={controls} args={[camera, domElement]}/>;
+}
+
 const Visualiser = props => {
   return (
     <Canvas className="visualiserCanvas" height="100%">
+      <CameraControls/>
       <ambientLight />
       {
         props.coords.map(coord => <Led position={coord} key={uuidv4()}/>)
