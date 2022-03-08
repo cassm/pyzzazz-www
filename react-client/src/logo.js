1f7849787a8775d06f38859adebd52e6cf487f1c
Cass May <cass@cassm.net>
2022-03-08 16:37:13 +0000

    use uuid in logo

--- react-client/src/logo.js
+++ react-client/src/logo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 const Logo = props => {
   const logoColour = "#750027";
@@ -10,16 +11,13 @@ const Logo = props => {
 
   const strokeWidth = props.size / 25;
   const lines = [];
-  if (!Logo.idIndex) {
-    Logo.idIndex = 0;
-  }
 
-  lines.push(<circle cx="50%" cy="50%" r="50%" fill="white" key={`logoMaskLine${Logo.idIndex++}`}/>);
+  lines.push(<circle cx="50%" cy="50%" r="50%" fill="white" key={uuidv4()}/>);
 
   for (let i = 0; i < 5; i++) {
     lines.push(<line x1="50%" y1="50%" x2="50%" y2="100%" stroke="black" strokeWidth={strokeWidth}
                      transform={`rotate(${i * fifthCircle} ${props.size * 0.5} ${props.size * 0.5})`}
-                     key={`logoMaskLine${Logo.idIndex++}`}/>);
+                     key={uuidv4()}/>);
 
     const innerPentIntersections = [[50 - 50 * innerPentRadius * radiusToEdge * 0.6, 50 * (1 - innerPentRadius)],
       [50 + 50 * innerPentRadius * radiusToEdge * 0.6, 50 * (1 - innerPentRadius)]];
@@ -34,54 +32,56 @@ const Logo = props => {
                      x2="50%" y2="0"
                      stroke="black" strokeWidth={strokeWidth}
                      transform={`rotate(${i * fifthCircle} ${props.size * 0.5} ${props.size * 0.5})`}
-                     key={`logoMaskLine${Logo.idIndex++}`}/>);
+                     key={uuidv4()}/>);
 
     lines.push(<line x1="50%" y1="0"
                      x2={`${innerPentIntersections[1][0]}%`} y2={`${innerPentIntersections[1][1]}%`}
                      stroke="black" strokeWidth={strokeWidth}
                      transform={`rotate(${i * fifthCircle} ${props.size * 0.5} ${props.size * 0.5})`}
-                     key={`logoMaskLine${Logo.idIndex++}`}/>);
+                     key={uuidv4()}/>);
 
     lines.push(<line x1={`${outerPentIntersections[0][0]}%`} y1={`${outerPentIntersections[0][1]}%`}
                      x2="50%" y2="0"
                      stroke="black" strokeWidth={strokeWidth}
                      transform={`rotate(${i * fifthCircle} ${props.size * 0.5} ${props.size * 0.5})`}
-                     key={`logoMaskLine${Logo.idIndex++}`}/>);
+                     key={uuidv4()}/>);
 
     lines.push(<line x1="50%" y1="0"
                      x2={`${outerPentIntersections[1][0]}%`} y2={`${outerPentIntersections[1][1]}%`}
                      stroke="black" strokeWidth={strokeWidth}
                      transform={`rotate(${i * fifthCircle} ${props.size * 0.5} ${props.size * 0.5})`}
-                     key={`logoMaskLine${Logo.idIndex++}`}/>);
+                     key={uuidv4()}/>);
 
     lines.push(<line x1={`${outestPentIntersections[0][0]}%`} y1={`${outestPentIntersections[0][1]}%`}
                      x2="50%" y2="0"
                      stroke="black" strokeWidth={strokeWidth}
                      transform={`rotate(${i * fifthCircle + fifthCircle / 2} ${props.size * 0.5} ${props.size * 0.5})`}
-                     key={`logoMaskLine${Logo.idIndex++}`}/>);
+                     key={uuidv4()}/>);
 
     lines.push(<line x1="50%" y1="0"
                      x2={`${outestPentIntersections[1][0]}%`} y2={`${outestPentIntersections[1][1]}%`}
                      stroke="black" strokeWidth={strokeWidth}
                      transform={`rotate(${i * fifthCircle + fifthCircle / 2} ${props.size * 0.5} ${props.size * 0.5})`}
-                     key={`logoMaskLine${Logo.idIndex++}`}/>);
+                     key={uuidv4()}/>);
 
 
     lines.push(<line x1={`${innerPentIntersections[0][0]}%`} y1={`${innerPentIntersections[0][1]}%`}
                      x2={`${innerPentIntersections[1][0]}%`} y2={`${innerPentIntersections[1][1]}%`}
                      stroke="black" strokeWidth={strokeWidth}
                      transform={`rotate(${i * fifthCircle} ${props.size * 0.5} ${props.size * 0.5})`}
-                     key={`logoMaskLine${Logo.idIndex++}`}/>);
+                     key={uuidv4()}/>);
   }
 
-  const radialGradientName = `logoRadialGradient${Logo.idIndex++}`
-  const maskName = `logoMask${Logo.idIndex++}`
+  const radialGradientName = `logoRadialGradient${uuidv4()}`
+  const maskName = `logoMask${uuidv4()}`
+  const fgColour = props.darkMode ? "white" : logoColour;
+
   return (
     <svg
       width={props.size} height={props.size}
       xmlns="http://www.w3.org/2000/svg"
       className="Logo"
-      key={`logoSvg${Logo.idIndex++}`}
+      key={uuidv4()}
     >
       <title>Pyzzazz Logo</title>
       <desc>An isometric view of a stellated icosahedron, rendered in white line-art over a radial gradient</desc>
@@ -96,7 +96,7 @@ const Logo = props => {
         </mask>
         mask>
       </defs>
-      <circle cx="50%" cy="50%" r="50%" fill={`url(#${radialGradientName})`} mask={`url(#${maskName})`} key={`logoGradientCircle${Logo.idIndex++}`}/>
+      <circle cx="50%" cy="50%" r="50%" fill={`url(#${radialGradientName})`} mask={`url(#${maskName})`} key={uuidv4()}/>
     </svg>
   )
 }
