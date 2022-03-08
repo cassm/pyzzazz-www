7470d774b20b36f5595061774a88933c80cb1cad
Cass May <cass@cassm.net>
2022-03-08 10:29:32 +0000

    use logger to log
    duh

--- routes/control.js
+++ routes/control.js
@@ -7,7 +7,7 @@ const r = redis.client.getInstance();
 
 router.post('/', async function(req, res) {
   const cmd = req.body;
-  console.log(cmd);
+  winston.debug(`client command received: ${JSON.stringify(cmd)}`);
   await r.rPush('pyzzazz:commands', JSON.stringify(cmd));
   res.sendStatus(201);
 });
