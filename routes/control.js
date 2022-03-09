2165b7f6a55015eb909365a2739c2777cce22e5b
Cass May <cass@cassm.net>
2022-03-09 13:32:42 +0000

    allow cors on POST and OPTIONS for /control

--- routes/control.js
+++ routes/control.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const redis = require('../src/db/redis');
 const winston = require('../src/utils/logger/winston');
 const router = express.Router();
 
 const r = redis.client.getInstance();
 
-router.post('/', async function(req, res) {
+router.options('/', cors({credentials: true, origin: true}));
+
+router.post('/', cors({credentials: true, origin: true}), async function(req, res, next) {
   const cmd = req.body;
   winston.debug(`client command received: ${JSON.stringify(cmd)}`);
   await r.rPush('pyzzazz:commands', JSON.stringify(cmd));
   res.sendStatus(201);
+  next();
 });
 
 module.exports = router;
