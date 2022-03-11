d2113ae0bbe4d5c3d57389b2c5b1b91bc02a6eee
Cass May <cass@cassm.net>
2022-03-11 14:39:29 +0000

    neaten up appbar styling

--- react-client/src/ResponsiveAppBar.js
+++ react-client/src/ResponsiveAppBar.js
@@ -7,6 +7,8 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Logo from "./logo";
+import ControlDrawer from "./ControlDrawer";
+import {Grid} from "@mui/material";
 
 const pages = ['status', 'configuration', 'control', 'visualiser'];
 
@@ -14,36 +16,34 @@ const ResponsiveAppBar = () => {
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
-        <Toolbar disableGutters sx={{alignItems: "center"}}>
-          <Box sx={{ml: 3, my: 2}}>
+        <Toolbar disableGutters>
+          <Grid container={true} wrap='nowrap' direction='row' justifyContent='space-around' gap='2rem' alignItems='center'>
             <Logo size="55" darkMode={true} gradient={false} key="navLogo"/>
-          </Box>
-          <Typography
-            variant="h2"
-            noWrap
-            component="div"
-            sx={{ ml: 3, display: { xs: 'none', md: 'flex' }, flexGrow: 1 }}
-          >
-            Pyzzazz
-          </Typography>
+            <Typography
+              variant="h2"
+              noWrap
+            >
+              Pyzzazz
+            </Typography>
 
-          <Box sx={{ display: { xs: 'none', md: 'flex' }, flexGrow: 1, justifyContent: "space-around" }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                component={Link}
-                to={`/${page}`}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                <Typography
-                  variant="h5"
-                  noWrap
+            <Box sx={{ display: { xs: 'none', md: 'flex' }, flexGrow: 1, justifyContent: "space-around" }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  component={Link}
+                  to={`/${page}`}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
                 >
-                {page}
-                </Typography>
-              </Button>
-            ))}
-          </Box>
+                  <Typography
+                    variant="h5"
+                    noWrap
+                  >
+                  {page}
+                  </Typography>
+                </Button>
+              ))}
+            </Box>
+          </Grid>
         </Toolbar>
       </Container>
     </AppBar>
